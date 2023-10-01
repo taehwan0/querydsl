e811d84a2277997e45b0b9fc4d9f2325b8a35e39
buddy <jang_taehwan@naver.com>
2023-10-01 13:02:23 +0900

    feat: querydsl setting and test
    - build.gradle 파악 필요함

--- src/test/java/com/example/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/example/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.querydsl;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.example.querydsl.entity.Hello;
+import com.example.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
 @SpringBootTest
 class QuerydslApplicationTests {
 
+    @Autowired
+    EntityManager em;
+
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+//        QHello qHello = new QHello("h");
+        QHello qHello = QHello.hello;
+
+        Hello result = query.
+            selectFrom(qHello)
+            .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
